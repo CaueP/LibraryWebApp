2066a743929452003f46ed603f5fe27d5b833a6b
Caue Polimanti <caue.polimanti@gmail.com>
2016-12-26 15:24:28 -0200

    Setting up ejs

--- app.js
+++ app.js
@@ -9,18 +9,22 @@ var port = process.env.PORT || 5000;
 /*
     setting up the middleware
 */
-// setting the public directory
+// setting the public directory for static files
 app.use(express.static('public'));
 
 // setting the views directory
-app.use(express.static('src/views'));
+//app.use(express.static('src/views'));
+app.set('views', './src/views');
+// setting ejs as the view engine
+app.set('view engine', 'ejs');
 
 /*
     setting the routes
 */
 // GET route for /
 app.get('/', function (req, res) {
-    res.send('Hello world');
+    // res.send('Hello world');
+    res.render('index', {title: 'Hello from render',lista: ['a', 'b']});
 });
 
 // GET route for /books
