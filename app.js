a4999fa30bd3e57b07075d479325d4807415e2ea
Caue Polimanti <caue.polimanti@gmail.com>
2017-01-05 12:28:03 -0200

    Creating Routes for Authentication - Modified index.ejs to hold our login page - SignUp POST method on authRoutes.js - Auth Routes configuration o app.js

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+// routes package
 var express = require('express');
+// package to parse the body of a request into a JSON object
+var bodyParser = require('body-parser');
 
 // gives an instance of express
 var app = express();
@@ -21,12 +24,18 @@ var nav = [
 // passing nav array of items
 var bookRouter = require('./src/routes/bookRoutes.js')(nav);
 var adminRouter = require('./src/routes/adminRoutes.js')();
+var authRouter = require('./src/routes/authRoutes.js')();
 
 /*
     setting up the middleware
 */
 // setting the public directory for static files
 app.use(express.static('public'));
+
+// parse application/json (parse the body into req.body for json encoded)
+app.use(bodyParser.json());
+// parse application/x-www-form-urlencoded (parse the body into req.body for url encoded)
+app.use(bodyParser.urlencoded({extended: false}));
 
 // setting the views directory
 app.set('views', './src/views');
@@ -36,6 +45,7 @@ app.set('view engine', 'ejs');
 // using the routes
 app.use('/Books', bookRouter);
 app.use('/Admin', adminRouter);
+app.use('/Auth', authRouter);
 
 /*
     setting the routes
