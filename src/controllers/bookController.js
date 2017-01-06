295c5fb7427f3088ab03b7a326513544bef7d4ef
Caue Polimanti <caue.polimanti@gmail.com>
2017-01-06 13:56:20 -0200

    Restructuring our bookRoutes in a Controller - Created a bookController

---
+++ src/controllers/bookController.js
@@ -0,0 +1,63 @@
+var mongodb = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+
+var bookController = function(bookService, nav) {
+
+    var middleware = function(req, res, next) {
+        /*if (!req.user) {
+            res.redirect('/');
+        }*/
+        next();
+    };
+
+    var getIndex = function(req, res) {
+
+            // mongodb server url
+            var url = 'mongodb://localhost:27017/libraryApp';
+
+            // connect to the database
+            mongodb.connect(url, function(err, db) {
+                var collection = db.collection('books');
+
+                // getting the collection of books and converting to JS array
+                collection.find({}).toArray(function(err, results) {
+                    res.render('bookListView', {
+                        title: 'Hello from render',
+                        nav: nav,
+                        books: results
+                    });
+                });
+            });
+        };
+
+    var getById = function (req, res) {
+        // get requested object id
+        var id = new ObjectId(req.params.id);
+
+        // mongodb server url
+        var url = 'mongodb://localhost:27017/libraryApp';
+
+        // connect to the database
+        mongodb.connect(url, function(err, db) {
+            var collection = db.collection('books');
+
+            // getting the collection of books and converting to JS array
+            collection.findOne({_id: id},
+                function(err, results) {
+                    res.render('bookView', {
+                        title: 'Books',
+                        nav: nav,
+                        book: results
+                    });
+                });
+        });
+    };
+
+    return {
+        getIndex: getIndex,
+        getById: getById,
+        middleware: middleware
+    };
+};
+
+module.exports = bookController;
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -7,61 +7,20 @@ var bookRouter = express.Router();
 // function router to receive nav item
 var router = function(nav) {
 
+    // importing controller
+    var bookController = require('../controllers/bookController')(null, nav);
+
     // secure all the routes, verifying if an user is authenticated before using any book route
-    bookRouter.use(function(req, res, next) {
-        if (!req.user) {
-            res.redirect('/');
-        }
-        next();
-    });
+    bookRouter.use(bookController.middleware);
 
     // Setting route for /Books
     bookRouter.route('/')
     // get method for /Books/ nd sending the json array of books
-        .get(function(req, res) {
-
-            // mongodb server url
-            var url = 'mongodb://localhost:27017/libraryApp';
-
-            // connect to the database
-            mongodb.connect(url, function(err, db) {
-                var collection = db.collection('books');
-
-                // getting the collection of books and converting to JS array
-                collection.find({}).toArray(function(err, results) {
-                    res.render('bookListView', {
-                        title: 'Hello from render',
-                        nav: nav,
-                        books: results
-                    });
-                });
-            });
-        });
+        .get(bookController.getIndex);
 
     // route to get a single book
     bookRouter.route('/:id')
-    .get(function (req, res) {
-        // get requested object id
-        var id = new ObjectId(req.params.id);
-
-        // mongodb server url
-        var url = 'mongodb://localhost:27017/libraryApp';
-
-        // connect to the database
-        mongodb.connect(url, function(err, db) {
-            var collection = db.collection('books');
-
-            // getting the collection of books and converting to JS array
-            collection.findOne({_id: id},
-                function(err, results) {
-                    res.render('bookView', {
-                        title: 'Books',
-                        nav: nav,
-                        book: results
-                    });
-                });
-        });
-    });
+    .get(bookController.getById);
 
     return bookRouter;
 };
